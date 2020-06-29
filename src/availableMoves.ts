740274159c06070b063474782baf40b2177e183f
Thomas Rory Gummerson <thomas@gummerson.no>
2020-06-29 16:19:53 +0200

    Better example

--- src/availableMoves.ts
+++ src/availableMoves.ts
@@ -1,7 +1,16 @@
 import { showBoard } from './cli/showBoard';
-import { initialBoard } from './primitives/initialBoard';
-import { availableMoves } from './primitives/Game';
+import { availableMoves, move } from './primitives/Game';
 import { initialGame } from './primitives/initialGame';
 
+const game2 = move(
+	{ from: 'a2', to: 'a3', piece: { set: 'white', kind: 'P' } },
+	initialGame
+);
+const game3 = move(
+	{ from: 'a7', to: 'a6', piece: { set: 'black', kind: 'P' } },
+	game2
+);
 console.log(availableMoves(initialGame));
-console.log(showBoard(initialBoard));
+console.log(showBoard(initialGame.board));
+console.log(availableMoves(game3));
+console.log(showBoard(game3.board));
